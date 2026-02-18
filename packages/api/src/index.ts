c424d7615fc5530aac70083352fc369897c69036
unknown <muhammadkhezar36@gmail.com>
2026-02-19 00:51:52 +0500

    updated packages/api/src/index.ts file

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -26,7 +26,7 @@ export * from './flow/manager';
 /* Middleware */
 export * from './middleware';
 /* Memory */
-export * from './memory';
+export * from './memory/index'
 /* Agents */
 export * from './agents';
 /* Prompts */
